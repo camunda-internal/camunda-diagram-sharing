2aaa581d91a4d3dadc7c49b846cfd2387052030a
damianwilczek <damian.wilczek@camunda.com>
2015-05-18 14:37:04 +0200

    chore(Web): change default upload Dropzone to elFinder

--- assets/js/login.js
+++
@@ -1,8 +0,0 @@
-﻿function Login(form) {
-	
-	var str = $(form).serialize(); 
-	$.ajax( { type: "POST", url: "login.php", data: str, success: function(msg){ 
-		location.reload();
-	}});
-	return false; 
-}
